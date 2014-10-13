37811de8c26be940b9c8ba86b9fa6ef062aa7c94
Ryan Wise <ryanw527@gmail.com>
2014-10-13 22:48:57 +0100

    Added base android files

---
+++ android/Jobs/src/com/jobs/activity/Main.java
@@ -0,0 +1,10 @@
+package com.jobs.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
