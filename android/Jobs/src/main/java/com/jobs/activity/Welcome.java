ad4892827cb1e117dd0086ac32f6efb8722ace48
Ryan Wise <ryanw527@gmail.com>
2014-11-01 04:01:44 +0100

    Recreated android projects to include gradle

---
+++ android/Jobs/src/main/java/com/jobs/activity/Welcome.java
@@ -0,0 +1,11 @@
+package com.jobs.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
